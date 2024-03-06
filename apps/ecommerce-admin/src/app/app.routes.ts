ef40dcb5a90cc047be941aebe202c8ee2735e9ba
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-03-06 20:26:56 -0300

    feat(user): add user component

--- apps/ecommerce-admin/src/app/app.routes.ts
+++ apps/ecommerce-admin/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'user',
+    loadChildren: () => import('user').then((m) => m.productDetailRoutes),
+  },
+];
---
+++ libs/modules/feature/user/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { UserComponent } from './user/user.component';
+
+export const productDetailRoutes: Route[] = [
+  { path: '', component: UserComponent },
+];
