f2714a0cdb8925624346f473ac0ae6f607b3c471
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-03-08 18:36:58 -0300

    test: should contain header

--- apps/ecommerce-admin/src/app/app.component.spec.ts
+++ apps/ecommerce-admin/src/app/app.component.spec.ts
@@ -1,27 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome ecommerce-admin'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Ecommerce');
   });
 
   it(`should have as title 'ecommerce-admin'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ecommerce-admin');
+  });
+
+  it('should contain header', () => {
+    const header: HTMLHeadingElement =
+      fixture.nativeElement.querySelector('header');
+    expect(header).toBeTruthy();
   });
 });
