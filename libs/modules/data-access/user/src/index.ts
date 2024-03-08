7224747328b35a88a789851c759f681d5f258c20
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-03-08 18:38:36 -0300

    feat(user-data-access): add mock

--- libs/modules/data-access/user/src/index.ts
+++ libs/modules/data-access/user/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/mocks/user.mock';
 export * from './lib/models/user.model';
 export * from './lib/services/list-users/list-users.service';
 export * from './lib/user-data-access.module';
