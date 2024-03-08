46f8f015c1ac8c419bda1642106b520bdbdd70ae
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-03-08 18:40:20 -0300

    test(user-data-access): test return users correctly in list users service

--- libs/modules/data-access/user/src/lib/services/list-users/list-users.service.spec.ts
+++ libs/modules/data-access/user/src/lib/services/list-users/list-users.service.spec.ts
@@ -1,16 +1,41 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { User } from '../../models/user.model';
 import { ListUsersService } from './list-users.service';
 
 describe('ListUsersService', () => {
   let service: ListUsersService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     TestBed.configureTestingModule({});
     service = TestBed.inject(ListUsersService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return users correctly', () => {
+    // ARRANGE
+    const url = `${service.apiUrl}/users?page=1&limit=6`;
+    let result: User[] = [];
+
+    // ACT
+    service.getUsers().subscribe((users) => (result = users));
+
+    // ASSERT
+    const request = httpMock.expectOne(url);
+    request.flush([]);
+    expect(request.request.method).toBe('GET');
+    expect(result).toEqual([]);
   });
 });
