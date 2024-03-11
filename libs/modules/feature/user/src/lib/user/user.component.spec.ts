d7033b6fa6655c98827f759092b73b082c2f94ff
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-03-11 20:07:27 -0300

    test(user): Dependency Inversion Principle

--- libs/modules/feature/user/src/lib/user/user.component.spec.ts
+++ libs/modules/feature/user/src/lib/user/user.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { ListUsersService, mockUsers } from 'user-data-access';
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
@@ -8,6 +10,12 @@ describe('UserComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [UserComponent],
+      providers: [
+        {
+          provide: ListUsersService,
+          useValue: { getUsers: () => of(mockUsers) },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserComponent);
